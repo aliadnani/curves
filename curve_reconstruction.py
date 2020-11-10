ac6817558cca033d007d3b3dabf44478c48e93e9
aliadnani <ali.adnani@hotmail.com>
2020-11-10 11:29:07 +0800

    Add timing

--- curve_reconstruction.py
+++ curve_reconstruction.py
@@ -4,12 +4,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import scipy.spatial as spatial
 from sklearn.neighbors import KDTree
 import vg
+import time
 
 
 # == FUNCTIONS ========================================================================================================
 
 # Takes points in [[x1, y1, z1], [x2, y2, z2]...] Numpy Array format
 def thin_line(points, point_cloud_thickness=0.53, iterations=1,sample_points=0):
+    total_start_time =  time.perf_counter()
     if sample_points != 0:
         points = points[:sample_points]
     
@@ -20,12 +22,17 @@ def thin_line(points, point_cloud_thickness=0.53, iterations=1,sample_points=0):
     new_points = []
     # Empty array for regression lines corresponding ^^ points
     regression_lines = []
-
+    nn_time = 0
+    rl_time = 0
+    prj_time = 0
     for point in point_tree.data:
         # Get list of points within specified radius {point_cloud_thickness}
+        start_time = time.perf_counter()
         points_in_radius = point_tree.data[point_tree.query_ball_point(point, point_cloud_thickness)]
+        nn_time += time.perf_counter()- start_time
 
         # Get mean of points within radius
+        start_time = time.perf_counter()
         data_mean = points_in_radius.mean(axis=0)
 
         # Calulate 3D regression line/principal component in point form with 2 coordinates
@@ -33,17 +40,25 @@ def thin_line(points, point_cloud_thickness=0.53, iterations=1,sample_points=0):
         linepts = vv[0] * np.mgrid[-1:1:2j][:, np.newaxis]
         linepts += data_mean
         regression_lines.append(list(linepts))
+        rl_time += time.perf_counter() - start_time
 
         # Project original point onto 3D regression line
+        start_time = time.perf_counter()
         ap = point - linepts[0]
         ab = linepts[1] - linepts[0]
         point_moved = linepts[0] + np.dot(ap,ab) / np.dot(ab,ab) * ab
-        new_points.append(list(point_moved))
+        prj_time += time.perf_counter()- start_time
 
+        new_points.append(list(point_moved))
+    print("--- %s seconds to thin points ---" % (time.perf_counter() - total_start_time))
+    print(f"Finding nearest neighbors for calculating regression lines: {nn_time}")
+    print(f"Calculating regression lines: {rl_time}")
+    print(f"Projecting original points on  regression lines: {prj_time}\n")
     return np.array(new_points), regression_lines
 
 # Sorts points outputed from thin_points()
 def sort_points(points, regression_lines, sorted_point_distance=0.2):
+    sort_points_time = time.perf_counter()
     # Index of point to be sorted
     index = 0
 
@@ -131,11 +146,13 @@ def sort_points(points, regression_lines, sorted_point_distance=0.2):
 
     # Combine 'sort_points_right' and 'sort_points_left'
     sort_points_right.extend(sort_points_left[::-1])
+    print("--- %s seconds to sort points ---" % (time.perf_counter() - sort_points_time))
     return np.array(sort_points_right)
 
 
 # == RUN SCRIPT ========================================================================================================
 
+start_time = time.perf_counter()
 
 # Generate spiral points with noise 
 total_rad = 10
@@ -148,7 +165,7 @@ x_true = np.cos(s_true)
 y_true = np.sin(s_true)
 z_true = s_true/z_factor
 
-num_sample_pts = 2000
+num_sample_pts = 1200
 s_sample = np.linspace(0, total_rad, num_sample_pts)
 x_sample = np.cos(s_sample) + noise * np.random.randn(num_sample_pts)
 y_sample = np.sin(s_sample) + noise * np.random.randn(num_sample_pts)
@@ -160,9 +177,17 @@ points = np.vstack((x_sample, y_sample, z_sample )).T
 # Shuffle points
 np.random.shuffle(points)
 
-# Run thinning and sorting algorithms
+generate_points_time = time.perf_counter()
+print(f'\nFor {len(x_sample)} points:')
+print("\n--- %s seconds to generate sample points ---\n" % (generate_points_time - start_time))
+
+# Thin & sort points
 thinned_points, regression_lines = thin_line(points)
+
+
 sorted_points = sort_points(thinned_points, regression_lines)
+
+# Run thinning and sorting algorithms
 
 # Plotting
 fig2 = plt.figure(2)
@@ -178,7 +203,7 @@ ax3d.plot(sorted_points.T[0], sorted_points.T[1], sorted_points.T[2], 'bo')
 ax3d.plot(sorted_points.T[0], sorted_points.T[1], sorted_points.T[2], '-b')
 
 # Plot thinned points
-ax3d.plot(thinned_points.T[0], thinned_points.T[1], thinned_points.T[2], 'go')
+# ax3d.plot(thinned_points.T[0], thinned_points.T[1], thinned_points.T[2], 'go')
 
 fig2.show()
 plt.show()
